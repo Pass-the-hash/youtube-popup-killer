8421831acd483cfa61c551af490f2ab55d58a873
Ioannis Mavrommatakis <51271363+Pass-the-hash@users.noreply.github.com>
2023-10-23 13:25:20 +0300

    Disable pause when video reaches end

--- youtube_popup_killer.js
+++ youtube_popup_killer.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         youtube popup killer
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.6
 // @description  try to take over the world!
 // @author       Pass the hash (original code written by Selbereth)
 // @match        https://*.youtube.com/*
